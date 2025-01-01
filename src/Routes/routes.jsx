5bdee77285bdacb18c45ff84eb4b02b08615e8c7
Mahdi Hassan Noor <contact.mahdiasif@gmail.com>
2025-01-01 16:51:14 +0600

    added menu item

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,29 +1,29 @@
-
 import {
     createBrowserRouter,
-   
   } from "react-router-dom";
-import Main from "../Layouts/Main";
-import Home from "../pages/Home";
-import Menu from "../pages/Menu";
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Menu from "../pages/Menu/Menu/Menu";
+import Order from "../pages/Order/Order/Order";
 
 
-
-
-  const router = createBrowserRouter([
+  export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
-      children:[
+      children: [
         {
-          path:'/',
-          element:<Home></Home>
-        },{
-          path:'/menu',
-          element:<Menu></Menu>
+            path: '/',
+            element: <Home></Home>
+        }, 
+        {
+          path: 'menu', 
+          element: <Menu></Menu>
+        },
+        {
+          path: 'order/:category',
+          element: <Order></Order>
         }
       ]
     },
   ]);
-
-export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { HelmetProvider } from "react-helmet-async";
-import { RouterProvider } from "react-router-dom";
-import router from './Routes/routes.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './Routes/Routes';
+import { HelmetProvider } from 'react-helmet-async';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div className='max-w-screen-xl mx-auto'>
-      <HelmetProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <div className='max-w-screen-xl mx-auto'>
         <RouterProvider router={router} />
-      </HelmetProvider>
-    </div>
-  </StrictMode>
-);
+      </div>
+    </HelmetProvider>
+  </React.StrictMode>,
+)

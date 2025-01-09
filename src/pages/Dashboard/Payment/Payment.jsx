3723d070189708931a33f235c679f4e854d26be2
Mahdi Hassan Noor <contact.mahdiasif@gmail.com>
2025-01-10 01:47:45 +0600

    installed payment stripe

---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,23 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle heading={'Payment'} subHeading={'Uira'} >
+                
+
+                
+             </SectionTitle>
+
+      <div>
+        <h2> taka o paki uira uira asho
+            
+        </h2>
+      </div>
+
+
+        </div>
+    );
+};
+
+export default Payment;
